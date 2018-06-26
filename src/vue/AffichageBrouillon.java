24825e57b421b8f5c98304cea7a5a4ee4fb247b5
vanteuton <quentin.duteil@gmail.com>
2018-06-26 16:47:07 +0200

    finish exo 4

---
+++ src/vue/AffichageBrouillon.java
@@ -0,0 +1,47 @@
+package vue;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import modele.Dessin;
+import modele.Etoile;
+import modele.Figure;
+import modele.Operation;
+import modele.Trace;
+
+public class AffichageBrouillon implements Operation {
+	private GraphicsContext gc;
+	public AffichageBrouillon(GraphicsContext context) {
+		gc = context;
+	}
+	public void opereSur(Dessin d) {
+		for(Figure f : d) f.realise(this);
+	}
+	public void opereSur(Trace t) {
+    	gc.setStroke(Color.BLACK);
+    	gc.setLineWidth(1);
+		double x, y;
+    	Point2D pt;
+    	IterateurBrouillon it = new IterateurBrouillon(t.iterator());
+    	pt = it.next();
+    	x = pt.getX();
+    	y = pt.getY();
+    	while (it.hasNext()) {
+    		pt = it.next();
+    		gc.strokeLine(x, y, pt.getX(), pt.getY());
+    		x = pt.getX();
+        	y = pt.getY();
+    	}
+	}
+	public void opereSur(Etoile e) {
+		IterateurBrouillon it = new IterateurBrouillon(e.iterator());
+    	gc.setStroke(Color.BLACK);
+    	gc.setLineWidth(1);
+		double xC = e.centre().getX(), yC = e.centre().getY();
+		Point2D pt;
+		while (it.hasNext()){
+		    pt = it.next();
+            gc.strokeLine(xC, yC, pt.getX(), pt.getY());
+        }
+	}
+}
--- src/vue/IterateurBrouillon.java
+++ src/vue/IterateurBrouillon.java
@@ -1,15 +1,32 @@
 package vue;
 
+import javafx.geometry.Point2D;
+
 import java.util.Iterator;
 
-public class IterateurBrouillon implements Iterator {
-    @Override
-    public boolean hasNext() {
-        return false;
+public class IterateurBrouillon implements Iterator<Point2D> {
+
+    private Iterator<Point2D> it_point;
+
+    IterateurBrouillon(Iterator<Point2D> iterator){
+        this.it_point = iterator;
     }
 
     @Override
-    public Object next() {
+    public boolean hasNext(){
+
+        if (it_point.hasNext()){
+            it_point.next();
+            return it_point.hasNext();
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public Point2D next() {
         return null;
     }
 }
